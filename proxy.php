415ac89221d7c4d992ac4151f9ff3caac847553a
IVO <Joshua.Ryan@gmx.us>
2019-12-14 05:06:23 -0800

    x
    x

--- proxy.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$url = isset($_GET[ 'url' ])? $_GET[ 'url' ] : null; 
-	$result = file_get_contents( $url );
-
-	header("Access-Control-Allow-Origin: *"); // Needed if you host the file separately from the startpage.
-	header('Content-type:application/rss+xml;charset=utf-8');
-	echo ( $result ) ;
-?>
